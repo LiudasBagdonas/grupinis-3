d11dff4ac118e53d3a40e3bded3fe52bec925eff
LaurisDesktop <glaurynasg@gmail.com>
2020-11-20 11:00:41 +0200

    Add navigation

--- bootloader.php
+++ bootloader.php
@@ -1,1 +1,14 @@
 <?php
+
+session_start();
+
+define('ROOT', __DIR__);
+define('DB_FILE', ROOT . '/app/data/db.json');
+
+require 'core/functions/html.php';
+require 'core/functions/file.php';
+require 'core/functions/auth.php';
+require 'core/functions/form/core.php';
+require 'core/functions/form/validators.php';
+require 'app/functions/form/validators.php';
+require 'app/functions/generators.php';
---
+++ public_html/users/cash-in.php
@@ -0,0 +1,27 @@
+<?php
+
+require '../../bootloader.php';
+
+$nav_array = nav();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../media/style.css">
+    <title>Cash-in</title>
+</head>
+
+<body>
+    <article class="wrapper">
+        <header>
+            <?php require ROOT . '/core/templates/nav.tpl.php' ?>
+        </header>
+    </article>
+</body>
+
+</html>
