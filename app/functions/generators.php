246fc4d01a14617f55d2d5901823c1b9e371312d
NikolajTaskin <nikolajtaskin@yahoo.com>
2020-11-20 13:00:09 +0200

    final commit, registration page

---
+++ app/functions/generators.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function nav()
+{
+    $nav_array = [
+        'Stats' => [
+            'path' => '../index.php',
+            'visible' => true,
+        ],
+        'Login' => [
+            'path' => '../login.php',
+            'visible' => true,
+        ],
+        'Register' => [
+            'path' => '../register.php',
+            'visible' => true,
+        ],
+        'Cash-in' => [
+            'path' => '../users/cash-in.php',
+            'visible' => true,
+        ],
+        'Play' => [
+            'path' => '../users/play.php',
+            'visible' => true,
+        ],
+        'Logout' => [
+            'path' => '../logout.php',
+            'visible' => true,
+        ],
+    ];
+
+    return $nav_array;
+}
